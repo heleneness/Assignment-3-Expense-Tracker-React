5eeca2b8212951419dc6d8382bc98a45c90baa32
Helene Ness <143997211+heleneness@users.noreply.github.com>
2024-02-15 14:52:55 +0100

    Added context and components

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 ### The purpose of this assignment is that the application should allow users to enter data about their expenses, delete expenses and display the registered expenses.
 
 React JavaScript Tutorial in Visual Studio Code
+
 npx create-react-app expence_tracker
-Inspired by Traversy Media - youTube - using hooks (useState, useContext, useReducer) and the context API.
+
+Inspired by Traversy Media - youTube - using hooks (useState, useContext, useReducer) 
+and the context API.
 
 Made the styling as I prefer. 
+Color palettes from colorswall.com - deep purple
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import Transaction from './Transaction';
+import { GlobalContext } from '../context/GlobalState';
 
-const TransactionList = () => {
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
 	<>
-	  <h3>History</h3>
+	  <h3>Title <span>Amount</span> </h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
 	</>
   )
 }
 
-export default TransactionList
+export default TransactionList;
